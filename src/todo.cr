aa7f30e28a30f3e2eef973e3017c1ea147f93b13
Arthur Poulet <arthur.poulet@mailoo.org>
2017-04-13 16:05:28 +0100

    Fix argument handling

--- src/todo.cr
+++ src/todo.cr
@@ -16,15 +16,18 @@ parsed = OptionParser.parse! do |p|
   p.on("-a=ID", "--archive=ID", "Move to archive") { |i| mode = :archive; id = i.to_i32 }
   p.on("-u=ID", "--update=ID", "Update the task ID") { |i| mode = :update; id = i.to_i32 }
   p.on("-r=ID", "--remove=ID", "Delete the task ID") { |i| mode = :rm; id = i.to_i32 }
-  p.on("-l=NAME", "--list=NAME", "Filter with the list NAME") { |name| list_name = name }
   p.on("-d=DATE", "--date=DATE", "Set the date") { |d| date = d }
+  p.on("--clear-all", "Remove everything in the list") { mode = :clear_all }
+
+  p.on("-l=NAME", "--list=NAME", "Filter with the list NAME") { |name| list_name = name }
+  p.on("--directory=PATH", "Change the directory of the todolist") { |path| dir_name = path }
+
   p.on("-s", "--sort", "Sort by date (by default)") { sort = :date }
   p.on("-i", "--sort-id", "Sort by id") { sort = :id }
-  p.on("--clear-all", "Remove everything in the list") { mode = :clear_all }
   p.on("-h", "--help", "Show this help") { puts p; exit }
-  p.unknown_args { |args| (mode = :add if mode.nil?; msg = args.join(" ")) unless args.empty? }
 end
 Dir.mkdir_p(dir_name)
-mode ||= :list
+msg = ARGV.join(" ").strip
+mode = !msg.empty? ? :add : :list if mode.nil?
 
 Todo::Exec.run(mode, date, id, list_name, dir_name, msg, sort)
